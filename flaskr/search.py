8d230423a8990cc08ef237fb28d3e8241b5d6f9c
Brady-Malott <brady.malott@gmail.com>
2021-03-27 03:29:08 -0400

    Setup nearbySearch api call and handled missing fields

--- flaskr/search.py
+++ flaskr/search.py
@@ -1,14 +1,15 @@
 import functools
+import requests
 
 from flask import (
     Blueprint, flash, redirect, render_template, request, url_for
 )
 
-bp = Blueprint('search', __name__)
+bp = Blueprint('searchBP', __name__)
 
 @bp.route('/')
 def index():
-  return redirect(url_for('search.search'))
+  return redirect(url_for('searchBP.search'))
 
 @bp.route('/search', methods=('GET', 'POST'))
 def search(vendors=None):
@@ -27,18 +28,34 @@ def info(text):
 
 def get_vendors(distance):
   # Make request to external api
-  data = [
-    {
-      'name': "joans barbecue and foot massage",
-      'address': '123 Main Street, Windsor',
-      'rating': 'Rating: 4.3',
-      'open': True,
-    },
-    {
-      'name': "Yusuf's rental carrots",
-      'address': '456 Main Street, Windsor',
-      'rating': 'Rating: 4.9',
-      'open': True,
-    },
-  ]
+
+  jsonResponse = requests.get(f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=42.041725972123416,-82.73100565945686&radius={distance}&key=AIzaSyAY0zWfgbZso6jkaj-ZLof79cj_NAyCk9k&type=meal_takeaway').json()
+
+  results = jsonResponse['results']
+
+  data = []
+
+  for item in results:
+    # Check if certain keys exist in the results
+
+    # opening_hours
+    if 'opening_hours' in item:
+      isOpen = item['opening_hours']['open_now']
+    else:
+      isOpen = False
+
+    # rating
+    if 'rating' in item:
+      rating = str(item['rating'])
+    else:
+      rating = 'No rating'
+    
+    place = {
+      'name': item['name'],
+      'address': item['vicinity'],
+      'rating': rating,
+      'open': isOpen
+    }
+    data.append(place)
+
   return data
